4cc796d3d9c0910fb7c787c5ae5dffff96894512
acrdlph <achill@convergent.cx>
2019-02-05 08:39:03 +0100

    backstage

--- src/components/Stories/Films/Film/Exclusive.js
+++ src/components/Stories/Films/Film/Exclusive.js
@@ -4,7 +4,7 @@ import classes from './Film.module.css'
 const exclusive = (props) => (
     <div className={classes.film}>
         <div style={{display: 'flex', alignItems: 'center'}}>
-            <iframe src="https://backstage.convergent.cx/#/production/0xf7c037215b858aB713B98fB572eB634c60e4c8cb" width="900" height="450"></iframe>
+        <iframe src="http://localhost:3000/#/production/0xf7c037215b858aB713B98fB572eB634c60e4c8cb" width="900" height="500"></iframe>
         </div>
     </div>
 
