38762c9911a1bbaa00933675b6121e0dea6dc8f8
acrdlph <achill@convergent.cx>
2019-03-16 18:48:24 +0100

    using only httpnot https

--- src/components/Stories/Films/Film/Exclusive.js
+++ src/components/Stories/Films/Film/Exclusive.js
@@ -4,7 +4,7 @@ import classes from './Film.module.css'
 const exclusive = (props) => (
     <div className={classes.film}>
         <div style={{display: 'flex', alignItems: 'center'}}>
-        <iframe src="http://localhost:3001/#/production/0xf7c037215b858aB713B98fB572eB634c60e4c8cb" width="900" height="500"></iframe>
+        <iframe src="http://d3i9owd1fcqee0.cloudfront.net/#/production/0xf7c037215b858aB713B98fB572eB634c60e4c8cb" width="900" height="500"></iframe>
         </div>
     </div>
 
